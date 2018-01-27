3d1a85a617d6a025b19ff42726c52f8d55b415e3
Kais Bettaieb <kais.bettaieb@esprit.tn>
2018-01-27 03:19:47 +0100

    added 404 not found when wanting to see a not validated ghouse or dont exist

--- src/ApplicationBundle/Controller/GhouseController.php
+++ src/ApplicationBundle/Controller/GhouseController.php
@@ -11,13 +11,18 @@ class GhouseController extends Controller
 {
     public function ghouseAction($id)
     {
-        $check_maison= $this->getDoctrine()
-        ->getRepository(Ghouse::class)
-        ->findOneBy(array('id' => $id));
-        if ($check_maison->getIsValidated() == 0){
-            return $this->render('@Application/404.html.twig');
+        $check_maison = $this->getDoctrine()
+            ->getRepository(Ghouse::class)
+            ->findOneBy(array('id' => $id));
+        if ($check_maison) {
+            if ($check_maison->getIsValidated() == 0) {
+                return $this->render('@Application/404.html.twig');
+            }
+            return $this->render('@Application/GhouseView/ghouse.html.twig', array('maison' => $check_maison));
         }
-        return $this->render('@Application/GhouseView/ghouse.html.twig', array('maison' => $check_maison));
+        return $this->render('@Application/404.html.twig');
+
+
     }
 
 }
